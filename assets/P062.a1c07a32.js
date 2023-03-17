236aecd4c97958cdef95258d02b34379b8e9312e
Marius Hauser <mariushauser@outlook.de>
2023-03-17 12:05:10 +0100

    adjusted paths and resources.json

--- assets/P062.a1c07a32.js
+++
@@ -1,5 +0,0 @@
-const e="062",a="Data Warehouse for ML",t="",n=`A data warehouse is required for mining/extracting new knowledge in the form of a database. A copy of an operational database may impede Data Mining Algorithms due to their structure. 
-
-One want to keep historic data (cleaned and uncleaned)`,o=`Specific strucutre of a Data Warehouse facilitates Data Mining Algorithms.
-
-Example: claimed data can be modified and corrected in the system. At the same time a Machine Learning algorithm can use this data to train.`,s="",i="",c=[""],r=["Architecture"],d=["012013"],m={id:e,name:a,aka:t,motivation:n,solution:o,consequences:s,examples:i,related:c,categories:r,resources:d};export{t as aka,r as categories,s as consequences,m as default,i as examples,e as id,n as motivation,a as name,c as related,d as resources,o as solution};
